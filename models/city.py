4c09f2205693c4f2e01ae88517dbd670b1a8edfe
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-07 17:56:20 -0800

    clone_v2

--- models/city.py
+++ models/city.py
@@ -7,3 +7,12 @@ class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+
+     __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities")
+
+   def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -6,7 +6,22 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
 
     def __init__(self, *args, **kwargs):
         """Define State"""
         super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """Define getter for list of city instances"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
