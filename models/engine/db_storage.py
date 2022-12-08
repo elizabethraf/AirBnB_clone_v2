30071ddf2bdbdbec13ce5edd329c4d9fdd871f53
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-12-07 17:45:58 -0800

    db_storage.py

---
+++ models/engine/db_storage.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""This is the DB storage class for AirBnB"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import sessionmaker, scoped_session
+import os
+from sqlalchemy import *
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+class DBStorage:
+    """interaacts with the MySQL database"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Display DBStorage Objest"""
+
+        HBNB_user = os.getenv('HBNB_MYSQL_USER')
+        HBNB_pwd = os.getenv('HBNB_MYSQL_PWD')
+        HBNB_host = os.getenv('HBNB_MYSQL_HOST')
+        HBNB_db = os.getenv('HBNB_MYSQL_DB')
+        HBNB_env = os.getenv('HBNB_ENV')
+
+         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                format(HBNB_MYSQL_USER,
+                HBNB_MYSQL_PWD,
+                HBNB_MYSQL_HOST,
+                HBNB_MYSQL_DB))
+
+        self.reload()
+        if HBNB_ENV == 'test':
+            Base.metadata.drop_all()
+
+    def all(self, cls=None):
+        """query on the current database session"""
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
+
+    def new(self, obj):
+        """add the object to the current database session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """commit all changes of the current database session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """reloads data from the database"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
+
