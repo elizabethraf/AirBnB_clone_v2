8d26752dcfc39a45c47002692f156678d34ac3ce
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-18 13:25:22 -0800

    models users

--- models/state.py
+++ models/state.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-
+from models import storage_t, storage
 
 class State(BaseModel):
     """ State class """
     name = ""
-    if models.storage_t == "db":
+    if storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
@@ -15,12 +15,12 @@ class State(BaseModel):
         """Define State"""
         super().__init__(*args, **kwargs)
 
-    if models.storage_t != "db":
+    if storage_t != "db":
         @property
         def cities(self):
             """Define getter for list of city instances"""
             city_list = []
-            all_cities = models.storage.all(City)
+            all_cities = storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
--- models/user.py
+++ models/user.py
@@ -12,16 +12,16 @@ import sqlalchemy
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user"
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user")
+    reviews = relationship("Review", backref="user")
 
-        else:
-            email = ' '
-            pswd = ' '
-            first_name = ' '
-            last_name = ' '
+    def __init__(self):
+        self.email = ' '
+        self.password = ' '
+        self.first_name = ' '
+        self.last_name = ' '
