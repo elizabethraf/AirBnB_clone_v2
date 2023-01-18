c3256c7b5823ba4779c3e6a4dd9625724afc4acd
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-18 04:26:34 -0800

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
+app.url_map.strict_slashes = False
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", methods=["GET"])
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
