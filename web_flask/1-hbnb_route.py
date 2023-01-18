1f67c474a49dffe8d475dbc118d9191703cfab26
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-18 10:40:11 -0800

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Display flask web application
-
+Display scriopt that start a flask web application
 """
 
 from flask import Flask
@@ -9,16 +8,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route('/', strict_slashes=False)
 def hello():
     """Display "Hello HBNB!" """
+
     return "Hello HBNB!"
 
-@app.route("/hbnb", methods=["GET"])
-def hbnb():
-     """Display “HBNB” """
-    return "HBNB"
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """Display "Hello HBNB!" """
+
+    return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
