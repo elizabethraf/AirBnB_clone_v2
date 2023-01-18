e369680fa224d7cdeb20d01e2173cbd87de01de5
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-18 10:52:13 -0800

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Display scriopt that start a flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display "Hello HBNB!" """
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def index():
+    """Display "Hello HBNB!" """
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text():
+    """Display “C”  """
+
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
