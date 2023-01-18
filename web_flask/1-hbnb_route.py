d480492310eabadf9cbcceb12f0f4cb1a2f58b97
elizabethraf <elizabethraf@matamaneholdings.co.za>
2023-01-18 10:33:38 -0800

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,16 +7,18 @@ Display flask web application
 from flask import Flask
 
 app = Flask(__name__)
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = False
-app.url_map.strict_slashes = False
+
 
 @app.route("/", methods=["GET"])
 def hello():
+    """Display "Hello HBNB!" """
     return "Hello HBNB!"
 
 @app.route("/hbnb", methods=["GET"])
 def hbnb():
+     """Display “HBNB” """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
